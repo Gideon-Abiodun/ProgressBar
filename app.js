edb55710c240545573dd5e05fa057452289ac6c1
Gideon-Abiodun <skuriangideon@gmail.com>
2023-07-25 13:54:10 +0100

    first push

---
+++ app.js
@@ -0,0 +1,11 @@
+const number = document.getElementById("number");
+let counter = 0;
+
+setInterval(()=>{
+    if(counter == "60"){
+        clearInterval();
+    }else{
+        counter += 1;
+        number.innerHTML = counter + "%";
+}
+}, 30);
